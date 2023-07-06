ea34fac4bd190d9fe8e50c28d067483bf7c72995
samini <soufianeamini2002@gmail.com>
2023-07-06 21:01:58 +0100

    made a trait for expression, and implemented two expressions, and their respective methods

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,58 @@ use std::io;
 
 mod lexer;
 
+mod parser {
+    use crate::lexer::Token;
+    pub struct Parser {
+        tokens: Vec<Token>,
+        current: Option<Token>,
+    }
+
+    impl Parser {
+        pub fn new(tokens: Vec<Token>) -> Parser {
+            Parser { tokens: tokens, current: None }
+        }
+
+        pub fn parse(&self) {}
+    }
+
+    trait Expression {
+        fn evaluate(&self) -> f64;
+    }
+
+    mod expression {
+        use super::Expression;
+
+        struct Literal {
+            value: f64,
+        }
+        
+        struct Binary {
+            operator: char,
+            left: Box<dyn Expression>,
+            right: Box<dyn Expression>,
+        }
+
+        impl Expression for Literal {
+            fn evaluate(&self) -> f64 {
+                self.value
+            }
+        }
+
+        impl Expression for Binary {
+            fn evaluate(&self) -> f64 {
+                match self.operator {
+                    '+' => self.left.evaluate() + self.right.evaluate(),
+                    '-' => self.left.evaluate() - self.right.evaluate(),
+                    '*' => self.left.evaluate() * self.right.evaluate(),
+                    '/' => self.left.evaluate() / self.right.evaluate(),
+                    _ => 0.,
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     loop {
         let mut line = String::new();
@@ -17,6 +69,8 @@ fn main() {
             break;
         }
         let tokens = lexer::lex(&line);
-        println!("{:?}", tokens);
+        let parser = parser::Parser::new(tokens);
+        parser.parse();
+        // println!("{:?}", tokens);
     }
 }
